8c45510997564e3c13a90aad18c754bc9a082e22
Dmitry Trofimov <dm.trofimov@corp.vk.com>
2021-11-21 17:18:41 +0300

    add broken test

--- src/test/java/Tests/SearchTests.java
+++ src/test/java/Tests/SearchTests.java
@@ -9,7 +9,6 @@ import static com.codeborne.selenide.Selenide.$$;
 import static com.codeborne.selenide.Selenide.open;
 
 public class SearchTests extends TestBase {
-//public class SearchTests {
 
     @Test
     void selenideSearchTest() {
@@ -24,6 +23,13 @@ public class SearchTests extends TestBase {
         open("https://www.google.com/");
         $(byName("q")).setValue("Selenide").pressEnter();
         $("#search").shouldHave(text("selenide.org"));
+        System.out.println($$(byClassName("logo")).size());
+    }
+
+    void selenideSearchTest3() {
+        open("https://www.google.com/");
+        $(byName("q")).setValue("Selenide").pressEnter();
+        $("#search").shouldHave(text("selenide1.org"));
         System.out.println($$(byClassName("logo")).size());
     }
 }
--- src/test/java/Tests/TestBase.java
+++ src/test/java/Tests/TestBase.java
@@ -1,22 +1,14 @@
 package Tests;
 
 import static com.codeborne.selenide.Selenide.closeWebDriver;
-import static com.codeborne.selenide.Selenide.open;
 import static com.codeborne.selenide.logevents.SelenideLogger.addListener;
 import static helpers.AttachmentHelper.*;
-import static helpers.AttachmentHelper.attachAsText;
-import static helpers.AttachmentHelper.attachPageSource;
-import static helpers.AttachmentHelper.attachScreenshot;
-import static helpers.AttachmentHelper.attachVideo;
-import static helpers.AttachmentHelper.getConsoleLogs;
-
 import com.codeborne.selenide.Configuration;
 import io.qameta.allure.selenide.AllureSelenide;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.openqa.selenium.remote.DesiredCapabilities;
-
 
 public class TestBase {
 
@@ -31,20 +23,9 @@ public class TestBase {
         Configuration.browser = "chrome";
         Configuration.headless = true;
 
-        // Configuration.browser = ownerConfig.getWebDriverBrowser();
         // Configuration.browserVersion = ownerConfig.getWebDriverBrowserVersion();
         Configuration.startMaximized = true;
 
-
-//        if(ownerConfig.isRemote()) {
-//        Configuration.remote = String.format("https://%s:%s@%s/wd/hub/",
-//                credentialConfig.remoteWebUser(),
-//                credentialConfig.remoteWebPassword(),
-//                credentialConfig.getSelenoidURL());
-//        }
-
-        //open(LOGIN_PAGE_URL);
-        //vkLoginPage.loginVK(credentialConfig.vkUserLogin(), credentialConfig.vkUserPassword());
     }
 
     @AfterAll
